2d797df08149c771c65641adba56c5319f805704
abuzar ghafari <abuzarr.ghafari@gmail.com>
2023-10-06 20:52:41 +0500

    Icons updated

--- resources/views/layouts/partials/leftsidebar.blade.php
+++ resources/views/layouts/partials/leftsidebar.blade.php
@@ -9,85 +9,46 @@
                 <li class="sidebar-item pt-2">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('home') }}"
                         aria-expanded="false">
-                        <i class="far ti-dashboard" aria-hidden="true"></i>
+                        <div class="svg-icon">@include('layouts.partials.svgs.dashboard')</div>
                         <span class="hide-menu">Dashboard</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('products.index') }}"
                         aria-expanded="false">
-                        <i class="fa ti-star" aria-hidden="true"></i>
+                        <div class="svg-icon">@include('layouts.partials.svgs.product')</div>
                         <span class="hide-menu">Products</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('areas.index') }}"
                         aria-expanded="false">
-                        <i class="fa ti-star" aria-hidden="true"></i>
+                        <div class="svg-icon">@include('layouts.partials.svgs.area')</div>
                         <span class="hide-menu">Areas</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('shop-types.index') }}"
                         aria-expanded="false">
-                        <i class="fa ti-star" aria-hidden="true"></i>
+                        <div class="svg-icon">@include('layouts.partials.svgs.shop-types')</div>
                         <span class="hide-menu">Shop Types</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('shops.index') }}"
                         aria-expanded="false">
-                        <i class="fa ti-star" aria-hidden="true"></i>
+                        <div class="svg-icon">@include('layouts.partials.svgs.shop')</div>
                         <span class="hide-menu">Shops</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('order-bookers.index') }}"
                         aria-expanded="false">
-                        <i class="fa ti-star" aria-hidden="true"></i>
+                        <div class="svg-icon">@include('layouts.partials.svgs.order-booker')</div>
                         <span class="hide-menu">Order Bookers</span>
                     </a>
                 </li>
-                {{-- <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="basic-table.html"
-                        aria-expanded="false">
-                        <i class="fa fa-table" aria-hidden="true"></i>
-                        <span class="hide-menu">Basic Table</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="fontawesome.html"
-                        aria-expanded="false">
-                        <i class="fa fa-font" aria-hidden="true"></i>
-                        <span class="hide-menu">Icon</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="map-google.html"
-                        aria-expanded="false">
-                        <i class="fa fa-globe" aria-hidden="true"></i>
-                        <span class="hide-menu">Google Map</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="blank.html"
-                        aria-expanded="false">
-                        <i class="fa fa-columns" aria-hidden="true"></i>
-                        <span class="hide-menu">Blank Page</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="404.html"
-                        aria-expanded="false">
-                        <i class="fa fa-info-circle" aria-hidden="true"></i>
-                        <span class="hide-menu">Error 404</span>
-                    </a>
-                </li> --}}
-                {{-- <li class="text-center p-20 upgrade-btn">
-                    <a href="https://www.wrappixel.com/templates/ampleadmin/"
-                        class="btn d-grid btn-danger text-white" target="_blank">
-                        Upgrade to Pro</a>
-                </li> --}}
+                
             </ul>
 
         </nav>
